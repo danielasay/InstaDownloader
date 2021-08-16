4397e23941abc724f553a1865c26687477ebfb56
danielasay <59574167+danielasay@users.noreply.github.com>
2021-08-16 16:32:52 -0400

    basic structure of actions

--- memes.py
+++ memes.py
@@ -1,7 +1,34 @@
 # Import instaloader module
 
 import instaloader
+import stdiomask
 
 # Create instance of the class
 
-bot = instaloader.Instaloader()
+meme_bot = instaloader.Instaloader()
+
+#Get username and password from user
+
+#username = input("Enter your username: ")
+#password = stdiomask.getpass()
+
+# While in Beta, will hardcode username and password
+
+username = input("Enter your username: ")
+password = stdiomask.getpass()
+
+meme_bot.login(username, password)
+
+## Load profile
+
+profile = instaloader.Profile.from_username(meme_bot.context, 'daquan')
+
+#Get the posts
+
+posts = profile.get_posts()
+
+## Iterate through each post and download them
+
+for index, post in enumerate(posts, 1):
+	meme_bot.download_post(post, target=f"{profile.username}_{index}")
+
