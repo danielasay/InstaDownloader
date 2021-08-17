020a844c81087c66f107b84d2271310ebad936e0
danielasay <59574167+danielasay@users.noreply.github.com>
2021-08-17 16:23:05 -0400

    now downloads posts into timestamped directory

--- memes.py
+++ memes.py
@@ -2,6 +2,8 @@
 
 import instaloader
 import stdiomask
+import os
+import datetime
 
 # Create instance of the class
 
@@ -17,6 +19,12 @@ meme_bot = instaloader.Instaloader()
 username = input("Enter your username: ")
 password = stdiomask.getpass()
 
+
+#Get the names of the pages desired
+
+user_input = input("Great! Now enter the names of the pages you want to see memes from, separated by spaces: ")
+pages = user_input.split(' ')
+
 meme_bot.login(username, password)
 
 ## Load profile
@@ -27,8 +35,11 @@ profile = instaloader.Profile.from_username(meme_bot.context, 'daquan')
 
 posts = profile.get_posts()
 
+
 ## Iterate through each post and download them
 
 for index, post in enumerate(posts, 1):
-	meme_bot.download_post(post, target=f"{profile.username}_{index}")
+	meme_bot.download_post(post, "daquan_" + str(datetime.date.today()))
+
+
 
