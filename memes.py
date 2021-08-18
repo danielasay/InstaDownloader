f22660ae499a757464dc02fd3db992013c5b8f10
danielasay <59574167+danielasay@users.noreply.github.com>
2021-08-18 16:20:03 -0400

    added date range functionality

--- memes.py
+++ memes.py
@@ -3,7 +3,9 @@
 import instaloader
 import stdiomask
 import os
-import datetime
+import datetime as dt
+from datetime import datetime
+from itertools import dropwhile, takewhile
 
 # Create instance of the class
 
@@ -11,35 +13,78 @@ meme_bot = instaloader.Instaloader()
 
 #Get username and password from user
 
-#username = input("Enter your username: ")
-#password = stdiomask.getpass()
-
-# While in Beta, will hardcode username and password
-
 username = input("Enter your username: ")
 password = stdiomask.getpass()
+
+# While in Beta, will hardcode username and password
 
 
 #Get the names of the pages desired
 
-user_input = input("Great! Now enter the names of the pages you want to see memes from, separated by spaces: ")
-pages = user_input.split(' ')
+meme_input = input("Great! Now enter the names of the pages you want to see memes from, separated by spaces: ")
+pages = meme_input.split(' ')
+
+#login to user's instagram account
 
 meme_bot.login(username, password)
 
-## Load profile
+## Load profile and get the posts
 
-profile = instaloader.Profile.from_username(meme_bot.context, 'daquan')
+posts = instaloader.Profile.from_username(meme_bot.context, pages[0]).get_posts()
 
-#Get the posts
+# Get the start date for posts range from user
 
-posts = profile.get_posts()
+input_date = input("How far back do you want to see posts from? Please enter in format mm/dd/yyyy: ")
+
+### Get today's date and change it into an integer
+
+today = dt.date.today()
+today = str(today)
+end_year = today[0:4]
+end_year = int(year)
+
+end_month = today[6:7]
+end_month = int(month)
+
+end_day = today[8:10]
+end_day = int(day)
+
+## populate range of dates
+
+start_date = datetime(2021, 8, 15)
 
 
-## Iterate through each post and download them
+end_date = datetime(end_year, end_month, end_day)
 
-for index, post in enumerate(posts, 1):
-	meme_bot.download_post(post, "daquan_" + str(datetime.date.today()))
+
+## Check the date of each post and download the posts that are in appropriate range
+
+for post in takewhile(lambda p: p.date > start_date, dropwhile(lambda p: p.date > end_date, posts)):
+    print(post.date)
+    meme_bot.download_post(post, pages[0] + str(dt.date.today()))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
