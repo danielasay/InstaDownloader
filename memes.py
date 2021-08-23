a962da9f15ed5ca30ab07007e0855e1144abd654
danielasay <59574167+danielasay@users.noreply.github.com>
2021-08-23 16:33:50 -0400

    Added sorting function by extenstion and print statements to follow progress

--- memes.py
+++ memes.py
@@ -6,6 +6,8 @@ import os
 import datetime as dt
 from datetime import datetime
 from itertools import dropwhile, takewhile
+import time
+import shutil as sh
 
 # Create instance of the class
 
@@ -18,13 +20,14 @@ password = stdiomask.getpass()
 
 # While in Beta, will hardcode username and password
 
-
 #Get the names of the pages desired
 
 meme_input = input("Great! Now enter the names of the pages you want to see memes from, separated by spaces: ")
 pages = meme_input.split(' ')
 
 #login to user's instagram account
+
+print("Logging into your Instagram account...")
 
 meme_bot.login(username, password)
 
@@ -34,24 +37,25 @@ posts = instaloader.Profile.from_username(meme_bot.context, pages[0]).get_posts(
 
 # Get the start date for posts range from user
 
-input_date = input("How far back do you want to see posts from? Please enter in format mm/dd/yyyy: ")
+#input_date = input("How far back do you want to see posts from? Please enter in format mm/dd/yyyy: ")
 
 ### Get today's date and change it into an integer
 
 today = dt.date.today()
 today = str(today)
 end_year = today[0:4]
-end_year = int(year)
+end_year = int(end_year)
 
 end_month = today[6:7]
-end_month = int(month)
+end_month = int(end_month)
 
 end_day = today[8:10]
-end_day = int(day)
+end_day = int(end_day)
+end_day = end_day + 1
 
 ## populate range of dates
 
-start_date = datetime(2021, 8, 15)
+start_date = datetime(2021, 8, 22)
 
 
 end_date = datetime(end_year, end_month, end_day)
@@ -59,23 +63,48 @@ end_date = datetime(end_year, end_month, end_day)
 
 ## Check the date of each post and download the posts that are in appropriate range
 
+print("Downloading posts between " + str(start_date) + " and " + str(end_date) + "...")
+
+time.sleep(4)
+
 for post in takewhile(lambda p: p.date > start_date, dropwhile(lambda p: p.date > end_date, posts)):
     print(post.date)
     meme_bot.download_post(post, pages[0] + str(dt.date.today()))
 
+print("Finished downloading!")
+
+### Navigate to newly created directory and create new sub directories
+
+time.sleep(2)
+
+print("Sorting media...")
+
+time.sleep(2)
+
+os.chdir("upworthy2021-08-23") ### make this dynamic
+os.makedirs("Captions")
+os.makedirs("Photos")
+os.makedirs("Videos")
+
+# Get list of all downloaded posts and turn them into strings
+
+data_list = os.listdir()
+
+for i in data_list:
+	i = str(i)
+	extension = i[-3:]
+	if extension == "mp4":
+		sh.move(i, "Videos")
+	elif extension == "jpg":
+		sh.move(i, "Photos")
+	elif extension == "txt":
+		sh.move(i, "Captions")
+	elif extension == ".xz":
+		os.remove(i)
 
 
 
-
-
-
-
-
-
-
-
-
-
+print("Done!")
 
 
 
